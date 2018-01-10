7bf2bc8bf59f445fc2e33d78d92c6c8c3aa8de12
Srirakshith Betageri <sbetageri111@gmail.com>
2018-01-10 12:53:47 -0600

    deleteNodeAtPosition

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -45,6 +45,40 @@ public class LinkedList {
         }
     }
 
+    public void deleteNodeAtPosition(int position) {
+        /*
+            Check if head is pointing to a list
+
+            if position is head, the move head to point to the next
+            else
+                if the next node is at the position:
+                    delete node
+                else:
+                    progress to next
+        */
+        if (head == null) {
+            return;
+        }
+
+        Node cur = head;
+
+        if (position == 1) {
+            head = head.next;
+        }
+
+        int curPosition = 1;
+        while (cur.next != null && curPosition + 1 != position) {
+            curPosition += 1;
+            cur = cur.next;
+        }
+
+        if (cur.next == null) {
+            return;
+        }
+
+        cur.next = cur.next.next;
+    }
+
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
